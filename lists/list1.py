569f75f4965fa57cadc7482c03fa748338606efd
muirurigrace12 <muirurigrace33@gmail.com>
2023-02-10 10:51:58 +0300

    python lists

---
+++ lists/list1.py
@@ -0,0 +1,125 @@
+#my first python list
+name = ["ace", "muiruri", "java", "python"]
+num = [1, 4, 6, 3, 5, 8, 17, 17, 12]
+
+
+#LIST METHODS
+#INDEX
+#print (num.index(6))
+#print (name.index("java"))
+
+#APPEND
+#before appending
+#print a list
+#for i in range(len(num)):
+   # print(num[i])
+
+#print("\n")
+#num.append(17)
+
+#after appending
+#for i in range(len(num)):
+    #print(num[i])
+
+#COUNT
+#element occurs in a list
+#print(num.count(17))
+
+#POP
+#it removes an element at a given index and returns that element
+#print(num.pop(2))
+
+#INSERT
+#num.insert(2.100)
+#for i in range(len(num)):
+    #print(num[i])
+
+#REMOVE 
+#removes the first occouence of an element from the list
+
+
+#SORT
+#arranges the list in an assending oder
+#for i in range(len(num)):
+   # print(num[i])
+
+#sort the list
+#num.sort()
+
+#add a new line for formatted output
+#print("\n")
+
+#for i in range(len(num)):
+    #print(num[i])
+
+#TIPS AND TRICKS
+
+#MULTIPLYIN A LIST
+#zeros
+
+#adding two lists
+#list1 = (89,90)
+#list2 = (45,78)
+#final list1 + list2 (89,90,45,78)
+
+#SLICING A LIST
+#nums = (12, 13, 34, 65,)
+#num = [2:]
+
+
+#getting max no
+#max(nums)
+
+#getting min no
+#min(nums)
+
+
+#CREATING LISTS
+
+even_nums = []
+
+for num in range(20):
+    if(num % 2)==0 :
+       even_nums.append(num)
+
+for i in even_nums:
+    print(even_nums[i])
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
